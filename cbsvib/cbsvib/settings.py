8c02d23ee5caba482691652d9dedadc9b1c2bc9d
Maria Skryabina <skryabina.mary98@gmail.com>
2023-06-02 11:29:08 +0300

    .env file added

--- cbsvib/cbsvib/settings.py
+++ cbsvib/cbsvib/settings.py
@@ -1,5 +1,6 @@
+import os
 from pathlib import Path
-
+import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,8 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bq*yv$-g96k02j+=!#fmp-uk=_2-sjne8@1dov$vb5ibpe3pxt'
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
+# UPDATE secret key
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
