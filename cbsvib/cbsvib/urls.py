e565dd527c5e052e0ad2077cc4aa520ae4fc856d
Maria Skryabina <skryabina.mary98@gmail.com>
2023-06-02 22:07:23 +0300

    changed css part for events section.

--- cbsvib/cbsvib/urls.py
+++ cbsvib/cbsvib/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from users import views as user_views
 
 
@@ -31,4 +32,8 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- cbsvib/events/views.py
+++ cbsvib/events/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Events
 
 
 def events_home(request):
-    main_data = {
-        'title': 'Мероприятия'
-    }
-    return render(request, 'events/events.html', main_data)
+    events = Events.objects.all()
+    return render(request, 'events/events.html', {"events":events})
+
+
